9feb83393ee3a0bcd8a8a91f0bcac4ffc7ebe903
AXDAC <515430273@qq.com>
2020-11-18 19:13:58 +0800

    new files

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python3 trainv2.py  --experiment ce_batch1 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD  --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss false --weight_type none
-
-
+./run_ce.sh
+./run_single_baseline.sh
+./run_baseline_global.sh
