1b6ddd1501ee493457b7ca5d3951d6d4831ed607
AXDAC <515430273@qq.com>
2020-11-23 07:00:31 +0800

    new script

--- results_summary/zhe_epochs_miou.py
+++ results_summary/zhe_epochs_miou.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from utils.tools import file_name
 
-#dir_name = ("./result/tmp")
-dir_name = ("./result/trace")
+dir_name = ("./result/tmp")
+#dir_name = ("./result/trace")
 x_axis=range(1,31)
-title ='ce losses with baseline'
+title ='focal losses with baseline'
 target_name = 'val_mIOU'
 
 file_array = file_name(dir_name)
 for file_name in file_array:
-    csv_data = pd.read_csv("./result/trace/{}".format(file_name))
+    csv_data = pd.read_csv("{}/{}".format(dir_name,file_name))
     cp1 = csv_data[target_name]
     l1=plt.plot(x_axis,cp1,label='{}'.format(file_name[:-4]))
 
