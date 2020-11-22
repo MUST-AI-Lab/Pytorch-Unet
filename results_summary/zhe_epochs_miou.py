926b68233fdfd0084d96c66d1b0449b1b72675eb
AXDAC <515430273@qq.com>
2020-11-22 23:38:23 +0800

    new files

--- results_summary/zhe_epochs_miou.py
+++ results_summary/zhe_epochs_miou.py
@@ -5,15 +5,17 @@ from utils.tools import file_name
 
 #dir_name = ("./result/tmp")
 dir_name = ("./result/trace")
-file_array = file_name(dir_name)
+x_axis=range(1,31)
+title ='ce losses with baseline'
+target_name = 'val_mIOU'
 
+file_array = file_name(dir_name)
 for file_name in file_array:
     csv_data = pd.read_csv("./result/trace/{}".format(file_name))
-    epoch=range(1,31)
-    cp1 = csv_data['val_mIOU']
-    l1=plt.plot(epoch,cp1,label='{}'.format(file_name[:-4]))
+    cp1 = csv_data[target_name]
+    l1=plt.plot(x_axis,cp1,label='{}'.format(file_name[:-4]))
 
-plt.title('cpmpairasion of losses ')
+plt.title('comparison of {}'.format(title))
 plt.xlabel('epoch')
 plt.ylabel('miou')
 plt.legend()
--- run.sh
+++ run.sh
@@ -1,11 +1,32 @@
 
-#python3 trainv3.py  --experiment trainv3_ce_baseline --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+python3 trainv3.py  --experiment trainv3_ce_baseline_repeat01 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
 
-#python3 trainv3.py  --experiment trainv3_focal_baseline --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss MultiFocalLossV4  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+python3 trainv3.py  --experiment trainv3_ce_baseline_repeat02 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
 
 
-python3 trainv3.py  --experiment DiceLossV3 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss DiceLossV3  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss false --weight_type none --device_id 0 --loss_reduce true
+python3 trainv3.py  --experiment trainv3_ce_baseline_repeat03 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
 
-python3 trainv3.py  --experiment ASLLoss --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss ASLLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss false --weight_type none --device_id 0 --loss_reduce true
 
-python3 trainv3.py  --experiment GDL --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss GDL  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss false --weight_type none --device_id 0 --loss_reduce true
+python3 trainv3.py  --experiment trainv3_ce_baseline_repeat04 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+
+
+python3 trainv3.py  --experiment trainv3_ce_baseline_repeat05 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+
+
+python3 trainv3.py  --experiment trainv3_ce_baseline_repeat06 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+
+
+
+python3 trainv3.py  --experiment trainv3_focal_baseline_repeat01 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss MultiFocalLossV4  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+
+python3 trainv3.py  --experiment trainv3_focal_baseline_repeat02 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss MultiFocalLossV4  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+
+python3 trainv3.py  --experiment trainv3_focal_baseline_repeat03 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss MultiFocalLossV4  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+
+python3 trainv3.py  --experiment trainv3_focal_baseline_repeat04 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss MultiFocalLossV4  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+
+python3 trainv3.py  --experiment trainv3_focal_baseline_repeat05 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss MultiFocalLossV4  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+
+python3 trainv3.py  --experiment trainv3_focal_baseline_repeat06 --dataset Cam2007Dataset --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss MultiFocalLossV4  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss true --weight_type single_baseline_weight --device_id 0
+
+
