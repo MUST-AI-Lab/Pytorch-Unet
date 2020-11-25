af50c6b935624ea9105b5fbf7a3f1248237e0182
AXDAC <515430273@qq.com>
2020-11-26 00:11:08 +0800

    new files

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
+python3 trainv3.py  --experiment single_global_ce_m0.4 --dataset Cam2007DatasetV2 --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.4 --save_check_point false  --force_save_last true  --weight_loss true --weight_type global_baseline_weight --device_id 0
 
-python3 train_tde.py  --experiment tde_ce --dataset Cam2007DatasetV2 --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNetTDE --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss false --weight_type none --device_id 0
-
-
-python3 train_tde.py  --experiment tde_focal --dataset Cam2007DatasetV2 --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNetTDE --deep_supervision false --optimizer SGD   --loss MultiFocalLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss false --weight_type none --device_id 0
-
-
-python3 train_tde.py  --experiment tde_eq --dataset Cam2007DatasetV2 --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNetTDE --deep_supervision false --optimizer SGD   --loss EqualizationLossV2  --epochs 30 --learning-rate 1e-2  --momentum 0.9 --save_check_point false  --force_save_last true  --weight_loss false --weight_type none --device_id 0
-
+python3 trainv3.py  --experiment single_global_ce_m95 --dataset Cam2007DatasetV2 --data_dir  ./data/Cam2007_n  --input_channels 3  --num_classes 32 --batch-size 1 --arch UNet --deep_supervision false --optimizer SGD   --loss WeightCrossEntropyLoss  --epochs 30 --learning-rate 1e-2  --momentum 0.95 --save_check_point false  --force_save_last true  --weight_loss true --weight_type global_baseline_weight --device_id 0
